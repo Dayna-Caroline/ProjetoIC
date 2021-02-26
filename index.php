32f3305de006d0102e3795be290558fa7e58c1fa
Dayna-Caroline <dayna.caroline@unesp.br>
2021-02-25 21:25:58 -0300

    Página HOME concluída

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+    <link rel="stylesheet" href="styles/home.css">
+</head>
+<body>
+    <div class="tudo">
+        <div class="cabecalho">
+            <div class="logo">
+                <img src="imgs/logo.png" alt="Logo da empresa" class="img-logo">
+                <h2>Smart Grids</h2>
+            </div>
+            <div class="pags">
+                <ul>
+                    <li><a href="">Entrar</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="corpo">
+            <div class="propaganda">
+                <h2>É viável implantar a smart grid na sua empresa?</h2>
+                <p>
+                    Use nosso site e encontre sua resposta.
+                </p>
+                <a href=""><button><h3>Usar &#8594;</h3></button></a>
+            </div>  
+    
+            <div class="funcionalidades">
+                <h2>Funcionalidades</h2>
+                <hr size=5>
+                <div class="infos">
+                    <div class="card">
+                        <div class="icon-card" style="color:rgb(0, 255, 128)">
+                            <i class="fas fa-plus"></i>
+                        </div>
+                        <h3>
+                            Cadastro de dados
+                        </h3>
+                        <div class="desc">
+                            <p>Cadastre sua empresa e os seus equipamentos elétricos.</p>
+                        </div>
+                    </div>
+    
+                    <div class="card">
+                        <div class="icon-card" style="color:rgb(0, 204, 255)">
+                            <i class="fas fa-stream"></i>
+                        </div>
+                        <h3>
+                            Gestão de projetos
+                        </h3>
+                        <div class="desc">
+                            <p>Defina e acompanhe os requistos para essa nova implantação.</p>
+                        </div>
+                    </div>
+    
+                    <div class="card">
+                        <div class="icon-card" style="color:rgb(255, 123, 0)">
+                            <i class="fas fa-cogs"></i>
+                        </div>
+                        <h3>
+                            Gestão de consumo
+                        </h3>
+                        <div class="desc">
+                            <p>Controle o consumo dos seus equipamentos.</p>
+                        </div>
+                    </div>
+    
+                    <div class="card">
+                        <div class="icon-card" style="color:rgb(195, 0, 255)">
+                            <i class="fas fa-chart-pie"></i>
+                        </div>
+                        <h3>
+                            Estátiscas e gráficos
+                        </h3>
+                        <div class="desc">
+                            <p>Confira os resultados da utilização da smart grid na sua empresa.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>   
+    
+            <div class="explicacao">
+                <img src="imgs/sg_explic.jpg" alt="Imagem Smart Grids">
+                <div class="explic">
+                    <h1>O que são smart grids?</h2>
+                    <p>São sistemas de distribuição e transmissão de energia elétrica que utilizam recursos digitais e da Tecnologia da Informação. Por meio delas, o sistema opera de forma mais eficiente, com maior controle do fluxo de energia e, consequentemente, passa a ser mais sustentável.</p>
+                </div>
+            </div>
+    
+            <div class="desenvolvedores">
+                <h2>Desenvolvedores</h2>
+                <hr size=5>
+                <div class="cards">
+                    <div class="bloco b1">
+                        <img src="imgs/kleber.jpg" alt="">
+                        <h3>Professor orientador</h3>
+                        <p>Kleber Rocha de Oliveira</p>
+                        <p>kleber.olivera@unesp.br</p>
+                    </div>
+                    <div class="bloco b1">
+                        <img src="imgs/dayna.jpg" width="194" alt="">
+                        <h3>Programadora</h3>
+                        <p>Dayna Caroline D. do Prado</p>
+                        <p>dayna.caroline@unesp.br</p>
+                    </div>
+                    <div class="bloco b1">
+                        <img src="imgs/eduardo.jpg" width="194" alt="">
+                        <h3>Programador</h3>
+                        <p>Eduardo Rodrigues</p>
+                        <p>eduardo.rodrigues2003@unesp.br</p>
+                    </div>
+                </div>  
+                <div class="cards">
+                    <div class="bloco b2">
+                        <img src="imgs/fer.jpg" width="194" alt="">
+                        <h3>Programadora</h3>
+                        <p>Fernanda Castor Modolo</p>
+                        <p>fernanda.modolo@unesp.br</p>
+                    </div>
+                    <div class="bloco b3">
+                        <img src="imgs/yoshio.jpg" width="194" alt="">
+                        <h3>Programador</h3>
+                        <p>Yoshio Masuyama Shibukawa</p>
+                        <p>yoshio.shibukawa@unesp.br</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr class="hr" size=5>
+
+        <div class="rodape">
+            <div class="logo">
+                <img src="imgs/logo.png" alt="Logo da empresa" class="img-logo">
+                <h2>Smart Grids</h2>
+            </div>
+            <div class="text">
+                Projeto de Iniciação científica da Universidade Estadual Paulista "Júlio de Mesquita Filho".
+            </div>
+        </div>
+    </div>
+</body>
+</html>
