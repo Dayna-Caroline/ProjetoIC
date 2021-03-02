3ca3eaa19cd0b46b8cc7bab442ac3d3a11ac08e2
Eduardo Rodrigues <eduardoHDA3.0@gmail.com>
2021-03-01 22:44:06 -0300

    login front

--- front/login.php
+++ front/login.php
@@ -1,33 +1,54 @@
 <!DOCTYPE html>
+
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/login.css">
-    <title>Login</title>
-</head>
-<body>
-    <div class="tudo">
-    <div class="cabecalho">
-        <div class="logo">
-            <a href="../index.php"><img src="../imgs/logo.png" alt="Logo da empresa" class="img-logo"></a>
-            <h2>Smart Grids</h2>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+        <link rel="stylesheet" href="../styles/login.css">
+        <title>Login</title>
+    </head>
+
+    <body>
+
+        <div class="tudo">
+
+            <div class="cabecalho">
+                <div class="logo">
+                    <a href="../index.php"><img src="../imgs/logo.png" alt="Logo da empresa" class="img-logo"></a>
+                    <h2>Smart Grids</h2>
+                </div>
+                <div class="pags">
+                    <ul>
+                        <li><a href="./cad.php">Criar Conta &#8594;</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <h1>Insira seus dados</h1>
+            <hr size=4>
+
+            <form class="form" action="../back/login.php" method="POST">
+                <div class="cnpj">
+                    <input type="CNPJ/CNAE" name="cnpjCnae" id="cnpjCnae" autocomplete="off" placeholder="CNPJ/CNAE" value="">
+                </div>
+
+                <div class="senha">
+                    <input type="password" name="senha" id="senha" placeholder="Senha" autocomplete="off">
+                    <button type="button" onclick="mostrarSenha()" class="ver"><i class="fas fa-eye"></i></button>
+                </div>
+                <input type="submit" name="btnLogar" class="botao" value="Login">
+            </form>
+
         </div>
-        
-    </div>
-    <form class="form" action="../back/login.php" method="POST">
-        <h1>Faça login</h1>
-        <div class="campo">
-            <input type="email" name="email" placeholder="Email" value="">
-        </div>
-        <div class="campo">
-            <input type="password" name="password" placeholder="Senha" autocomplete="off">
-        </div>
-        <input type="submit" name="btnLogar" value="Login">
-    </form>
-    </div>
-</body>
+
+        <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+        <script src="../js/jquery-1.2.6.pack.js" type="text/javascript"></script>
+        <script src="../js/jquery.maskedinput-1.1.4.pack.js" type="text/javascript"></script>
+        <script src="../js/funcs_login.js"></script>
+
+    </body>
+
 </html>
