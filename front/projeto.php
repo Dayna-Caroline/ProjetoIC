abc1802b6abbf19887b08d1055f482d973633e85
Eduardo Rodrigues <eduardoHDA3.0@gmail.com>
2021-03-17 22:59:38 -0300

    projetos

--- front/projeto.php
+++ front/projeto.php
@@ -6,6 +6,7 @@
     $query = "SELECT * FROM projeto WHERE empresa = '{$_SESSION['id_empresa']}' AND md5(id_projeto) = '{$_GET['id']}';";
 
     // executa a query
+
     $result = mysqli_query($conecta, $query);
     $row = mysqli_num_rows($result);
     if($row>0)
@@ -53,13 +54,13 @@
                     <h2>Smart Grids</h2>
                 </div>
                 <ul>
-                    <li><a href="empresa.php"><i class="fas fa-city"></i></i><span class="nav-text">Empresa</span></a></li>
-                    <li class="pag"><a href="menu.php?pagina=1"><i class="fas fa-stream"></i></i><span class="nav-text">Projetos</span></a></li>
-                    <li><a href="funcionarios.php?pagina=1"><i class="fas fa-users"></i><span class="nav-text">Funcionários</span></a></li>
-                    <li><a href=""><i class="fas fa-battery-three-quarters"></i><span class="nav-text">Equipamentos</span></a></li>
-                    <li><a href="requisitos.php"><i class="fas fa-edit"></i><span class="nav-text">Requisitos</span></a></li>
-                    <li><a href=""><i class="fas fa-cogs"></i><span class="nav-text">Controle</span></a></li>
-                    <li><a href=""><i class="fas fa-chart-pie"></i><span class="nav-text">Resultados</span></a></li>
+                    <li class="navitem"><a href="empresa.php"><i class="fas fa-city"></i><span class="nav-text">Empresa</span></a></li>
+                    <li class="pag navitem"><a href="menu.php?pagina=1"><i class="fas fa-stream"></i><span class="nav-text">Projetos</span></a></li>
+                    <li class="navitem"><a href="funcionarios.php?pagina=1"><i class="fas fa-users"></i><span class="nav-text">Funcionários</span></a></li>
+                    <li class="navitem"><a href=""><i class="fas fa-battery-three-quarters"></i><span class="nav-text">Equipamentos</span></a></li>
+                    <li class="navitem"><a href="requisitos.php"><i class="fas fa-edit"></i><span class="nav-text">Requisitos</span></a></li>
+                    <li class="navitem"><a href=""><i class="fas fa-cogs"></i><span class="nav-text">Controle</span></a></li>
+                    <li class="navitem"><a href=""><i class="fas fa-chart-pie"></i><span class="nav-text">Resultados</span></a></li>
                 </ul>
             </div>
 
@@ -73,58 +74,54 @@
                 <div class="projetos2">
                     <?php
                         echo "<div class=\"item2\">
-                            <div style=\"color:rgb(92, 91, 91);cursor:default;\" class=\"leg-id2\"><b>ID Projeto</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" disabled value=\"".$id."\"></div>
+                            <div style=\"color:#999999;cursor:default;\" class=\"leg-id2\"><b>ID Projeto</b></div>
+                            <div style=\"cursor:not-allowed;\" class=\"item-id2\"><input style=\"cursor:not-allowed;\" type=\"text\" disabled value=\"".$id."\"></div>
                         </div>";
                         
                         echo "<div class=\"item2\">
-                            <div style=\"color:rgb(92, 91, 91);cursor:default;\" class=\"leg-id2\"><b>ID Empresa</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" disabled value=\"".$id_empresa."\"></div>
+                            <div style=\"color:#999999;cursor:default;\" class=\"leg-id2\"><b>ID Empresa</b></div>
+                            <div style=\"cursor:not-allowed;\" class=\"item-id2\"><input type=\"text\" style=\"cursor:not-allowed;\" disabled value=\"".$id_empresa."\"></div>
                         </div>";
 
                         echo "<div class=\"item2\">
                             <div class=\"leg-id2\"><b>ID Responsável</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" value=\"".$responsavel."\"></div>
+                            <div class=\"item-id2\"><input type=\"text\" name=\"responsavel\" value=\"".$responsavel."\"></div>
                         </div>";
 
                         echo "<div class=\"item2\">
                             <div class=\"leg-id2\"><b>Descrição do projeto</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" value=\"".$descricao."\"></div>
+                            <div class=\"item-id2\"><input type=\"text\" name=\"descricao\" value=\"".$descricao."\"></div>
                         </div>";
 
                         echo "<div class=\"item2\">
                             <div class=\"leg-id2\"><b>Finalidade do projeto</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" value=\"".$finalidade."\"></div>
+                            <div class=\"item-id2\"><input type=\"text\" name=\"finalidade\" value=\"".$finalidade."\"></div>
                         </div>";
 
                         echo "<div class=\"item2\">
-                            <div class=\"leg-id2\"><b>Orçamento</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" value=\"".$orcamento."\"></div>
-                        </div>";
-
-                        echo "<div class=\"item2\">
-                            <div class=\"leg-id2\"><b>Data de Início</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" value=\"".$inicio."\"></div>
-                        </div>";
-
-                        echo "<div class=\"item2\">
-                            <div class=\"leg-id2\"><b>Data de Aprovação</b></div>
-                            <div class=\"item-id2\"><input type=\"text\" value=\"".$aprovacao."\"></div>
-                        </div>";
-
-                        if($ativo!='s'){
-
-                            echo "<div class=\"item2\">
-                                <div class=\"leg-id2\"><b>Data do término</b></div>
-                                <div class=\"item-id2\"><input type=\"text\" value=\"".$fim."\"></div>
-                            </div>";
-
-                            echo "<div class=\"item2\">
-                                <div class=\"leg-id2\"><b>Custo final</b></div>
-                                <div class=\"item-id2\"><input type=\"text\" value=\"".$c_final."\"></div>
-                            </div>";
+                        
+                            <div class=\"leg-id2\"><b>Orçamento (R$)</b></div>
+                            <div style=\"width:140px;\" class=\"item-id2\"><input class=\"numero\" type=\"number\" name=\"orcamento\" step=\".01\" value=\"".$orcamento."\"></div>
                             
-                        }
+                            <div class=\"leg-id2\" style=\"margin-right: 10px;\"><b>Custo final (R$)</b></div>
+                            <div style=\"width:140px;\" class=\"item-id2\"><input class=\"numero\" type=\"number\" step=\".01\" name=\"c_final\" value=\"".$c_final."\"></div>
+                        
+                        </div>";
+
+                        echo "<div class=\"item2\">
+                        
+                            <div class=\"leg-id2\"><b>Data de Início</b></div>
+                            <div style=\"width:150px;\" class=\"item-id2\"><input name=\"inicio\" type=\"date\" value=\"".$inicio."\"></div>
+                            
+                            <div class=\"leg-id2\"><b>Data de Aprovação</b></div>
+                            <div style=\"width:150px;\" class=\"item-id2\"><input name=\"aprovacao\" type=\"date\" value=\"".$aprovacao."\"></div>
+                        
+                        </div>";
+
+                        echo "<div class=\"item2\">
+                            <div class=\"leg-id2\"><b>Data do término</b></div>
+                            <div style=\"width:150px;\" class=\"item-id2\"><input name=\"fim\" type=\"date\" value=\"".$fim."\"></div>
+                        </div>";
 
                     ?>
                 </div>
