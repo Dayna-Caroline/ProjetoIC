17b03b5dd35ca8220c028d33d3ff65c2c37b05f8
Eduardo <eduardoHDA3.0@gmail.com>
2021-08-23 21:58:03 -0300

    aaa

--- front/controle/consumo.php
+++ front/controle/consumo.php
@@ -13,8 +13,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
-    <link rel="stylesheet" href="../../styles/consumo/consumo.css">
     <link rel="stylesheet" href="../../styles/projetos/menu.css">
+    <link rel="stylesheet" href="../../styles/consumo/consumo.css">
     <title>Smart Grid</title>
 </head>
 <body onclick="verifica()" onload="verifica()">
